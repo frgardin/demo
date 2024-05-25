e1484aec6d5c528c03bf2c04a95f19fe3cf1874e
frgardin <gardinfelipe@gmail.com>
2024-05-24 22:15:34 -0300

    fixing controller

--- src/main/java/com/learning/coursera/demo/controller/HelloController.java
+++ src/main/java/com/learning/coursera/demo/controller/HelloController.java
@@ -1,9 +1,11 @@
 package com.learning.coursera.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/hello")
+@RestController
+@RequestMapping("/hello")
 public class HelloController {
 
     @GetMapping
