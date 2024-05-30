29b4e02de4b19bd1cf97ce4b06161ad4b5c85cf9
Felipe Gardin <gardinfelipe@gmail.com>
2024-05-30 06:20:49 -0300

    configuration properties

---
+++ src/main/java/com/example/demo/controller/FunRestController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/fun")
+    public String getFun() {
+        return coachName + " " + teamName;
+    }
+}
